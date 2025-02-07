c964f0b525dcaf8fa711a2340574f70921cfa634
Sebastian Kastrul <sebasian.kastrul@gmail.com>
2025-02-07 13:34:43 -0700

    Implemented basic page navigation structure for home,save,fetch pages

--- node-api/client/src/App.js
+++ node-api/client/src/App.js
@@ -1,25 +1,65 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from "react";
+
+function Home({handlePageChange}) {
+  return(
+    <div>
+      <h1>Home Page</h1>
+      <NavigationArea handlePageChange={handlePageChange} />
+    </div>
+  )
+}
+
+function Save({handlePageChange}) {
+  return(
+    <div>
+      <h1>Save Page</h1>
+      <NavigationArea handlePageChange={handlePageChange} />
+    </div>
+  )
+}
+
+function Fetch({handlePageChange}) {
+  return(
+    <div>
+      <h1>Fetch Page</h1>
+      <NavigationArea handlePageChange={handlePageChange} />
+    </div>
+  )
+}
+
+function NavigationArea({handlePageChange}) {
+  return(
+    <div class = "navigation">
+        <button onClick={() => handlePageChange('home')}>Home</button>
+        <button onClick={() => handlePageChange('save')}>Save</button>
+        <button onClick={() => handlePageChange('fetch')}>Fetch</button>
+    </div>
+  )
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // set default state to home
+  const [page, setPage] = useState("home");
+  // onclick page change handler
+  const handlePageChange = ( newPageState ) => {
+    setPage(newPageState);
+  }
+  // render correct page based on page state
+  if( page == "home"){
+    return (
+      <Home handlePageChange={ handlePageChange }/>
+    );
+  } else if ( page == "save" ) {
+    return (
+      <Save handlePageChange={ handlePageChange }/>
+    )
+  } else if ( page == "fetch" ) {
+    return(
+      <Fetch handlePageChange={ handlePageChange}/>
+    )
+  }
 }
 
 export default App;
